ad4f1d849370b7b83948b56772d1f75e0d739b66
kmgleasonc <67933244+kmgleasonc@users.noreply.github.com>
2025-03-05 20:03:03 -0800

    Add files via upload

---
+++ Assignment06.py
@@ -0,0 +1,202 @@
+# ------------------------------------------------------------------------------------------ #
+# Title: Assignment06_Starter
+# Desc: This assignment demonstrates using functions
+# with structured error handling
+# Change Log: (Who, When, What)
+#   RRoot,1/1/2030,Created Script
+#   <Keily Gleason>,<3/1/2025>,<Functions>
+# ------------------------------------------------------------------------------------------ #
+import json
+
+# Define the Data Constants
+MENU: str = '''
+---- Course Registration Program ----
+  Select from the following menu:  
+    1. Register a Student for a Course.
+    2. Show current data.  
+    3. Save data to a file.
+    4. Exit the program.
+----------------------------------------- 
+'''
+
+FILE_NAME: str = "Enrollments.json"
+students: list = []  # a table of student data
+menu_choice: str  # Hold the choice made by the user.
+
+# Processing --------------------------------------- #
+class FileProcessor:
+    """
+    A collection of processing layer functions that work with Json files
+
+    ChangeLog: Keily Gleason, March 4 2025, Created Class
+    """
+
+    # When the program starts, read the file data into table
+    # Extract the data from the file
+    # Read from the Json file
+
+    @staticmethod
+    def read_data_from_file(file_name: str, student_data: list):
+        try:
+            file = open(file_name, "r")
+            student_data = json.load(file)
+            file.close()
+        except FileNotFoundError as e:
+            IO.output_error_messages("Text file must exist before running this script!", e)
+        except Exception as e:
+            IO.output_error_messages("There was a non-specific error!", e)
+        finally:
+            if file.closed == False:
+                file.close()
+        return student_data
+
+    @staticmethod
+    def write_data_to_file(file_name: str, student_data: list):
+        # global file
+        # global students
+
+        try:
+            file = open(file_name, "w")
+            json.dump(student_data, file)
+            file.close()
+        except TypeError as e:
+            IO.output_error_messages("Please check that the data is a valid JSON format", e)
+        except Exception as e:
+            IO.output_error_messages("There was a non-specific error!", e)
+        finally:
+            if file.closed == False:
+                file.close()
+
+# Presentation --------------------------------------- #
+class IO:
+    """
+    A collection of presentation layer functions that manage user input and output
+
+    ChangeLog: Keily Gleason, March 4 2025
+    Created Class
+    Added menu output and input functions
+    Added a function to display the data
+    Added a function to display custom error messages
+    """
+    pass
+
+    @staticmethod
+    def output_error_messages(message: str, error: Exception = None):
+        """ This function displays the a custom error messages to the user
+
+        ChangeLog: (Who, When, What)
+        RRoot,1.3.2030,Created function
+
+        :return: None
+        """
+        print(message, end="\n\n")
+        if error is not None:
+            print("-- Technical Error Message -- ")
+            print(error, error.__doc__, type(error), sep='\n')
+
+    @staticmethod
+    def output_menu(menu: str):
+        """ This function displays the a menu of choices to the user
+
+        ChangeLog: (Who, When, What)
+        RRoot,1.3.2030,Created function
+
+        :return: None
+        """
+        print()
+        print(menu)
+        print()  # Adding extra space to make it look nicer.
+
+    @staticmethod
+    def input_menu_choice():
+        """ This function gets a menu choice from the user
+
+        :return: string with the users choice
+        """
+        choice = "0"
+        try:
+            choice = input("Enter your menu choice number: ")
+            if choice not in ("1", "2", "3", "4"):  # Note these are strings
+                raise Exception("Please, choose only 1, 2, 3, or 4")
+        except Exception as e:
+            IO.output_error_messages(e.__str__())  # Not passing the exception object to avoid the technical message
+
+        return choice
+
+    @staticmethod
+    def output_student_courses(student_data: list):
+        # Process the data to create and display a custom message
+        print()
+        print("-" * 50)
+        for student in students:
+            print(f'Student {student["FirstName"]} '
+                  f'{student["LastName"]} is enrolled in {student["CourseName"]}')
+        print("-" * 50)
+        print()
+
+    @staticmethod
+    def input_student_data(student_data: list):
+        """ This function gets the first name, last name, and GPA from the user
+
+        ChangeLog: Keily Gleason, March 4 2025
+        Created function
+
+        :return: None
+        """
+
+        try:
+            # Input the data
+            student_first_name = input("What is the student's first name? ")
+            if not student_first_name.isalpha():
+                raise ValueError("The first name should not contain numbers.")
+
+            student_last_name = input("What is the student's last name? ")
+            if not student_last_name.isalpha():
+                raise ValueError("The last name should not contain numbers.")
+
+            course_name = input("Please enter the name of the course: ")
+            student_data = {"FirstName": student_first_name,
+                            "LastName": student_last_name,
+                            "CourseName": course_name}
+            students.append(student_data)
+            print(f"You have registered {student_first_name} {student_last_name} for {course_name}.")
+        except ValueError as e:
+            print(e)  # Prints the custom message
+            print("-- Technical Error Message -- ")
+            print(e.__doc__)
+            print(e.__str__())
+        except Exception as e:
+            print("Error: There was a problem with your entered data.")
+            print("-- Technical Error Message -- ")
+            print(e.__doc__)
+            print(e.__str__())
+        return students
+
+#  End of function definitions
+
+# Beginning of the main body of this script
+students = FileProcessor.read_data_from_file(file_name=FILE_NAME, student_data=students)
+
+# Repeat the follow tasks
+while True:
+    IO.output_menu(menu=MENU)
+
+    menu_choice = IO.input_menu_choice()
+
+    if menu_choice == "1":  # Get new data (and display the change)
+        students = IO.input_student_data(student_data=students)
+        IO.output_student_courses(student_data=students)  # Added this to improve user experience
+        continue
+
+    elif menu_choice == "2":  # Display current data
+        IO.output_student_courses(student_data=students)
+        continue
+
+    elif menu_choice == "3":  # Save data in a file
+        FileProcessor.write_data_to_file(file_name=FILE_NAME, student_data=students)
+        continue
+
+    elif menu_choice == "4":  # End the program
+        break  # out of the while loop
+
+print("Program Ended")
